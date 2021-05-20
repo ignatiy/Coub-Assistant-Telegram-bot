9699671f73f962c3f1f25768ef946080c0cbaa3c
nonadminus <ignatii1994-23-10.ru@mail.ru>
2021-05-20 13:45:13 +0700

    update config.py

--- app/config.py
+++ app/config.py
@@ -1,14 +1,14 @@
 # Ссылка на страницу сайта который будем парсить с капчей
 PAGE_URL = "https://coubassistant.com/"
 # token BotFather
-TOKEN = "token"
+TOKEN = "1762361955:AAG2hiIZ9Bw7xYK5jm3yMyrbz8rrNMO2ufU"
 # url api telegram или на прокси сервер
 PROXY = "https://api.telegram.org/bot"
 # Ключ от рукапчи из своего аккаунта https://rucaptcha.com
-RUCAPTCHA_KEY = "key"
+RUCAPTCHA_KEY = "ef35fba55008aae418ca908a27bd29b0"
 # Google sitekey можно найти на странице coubassistant
-SITE_KEY = "sitekey"
+SITE_KEY = "6Lej9tkaAAAAAKWusqMcrOeA-hpsHlgjXu-feuGF"
 # Значение параметра action, которые вы нашли в коде сайта
-ACTION = "action"
+ACTION = "homepage"
 # Требуемое значение рейтинга (score) работника, от 0.1(робот) до 0.9(человечный человек)
 MIN_SCORE = 0.8
